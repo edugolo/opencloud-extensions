039b2e1f9d3713b15940a7c0522eda89b0286e3f
Johan Coppens <johan.coppens@edugolo.be>
2026-04-30 09:28:08 +0200

    chore: prepare workspace for vue+vite packages
    Eslint config splitst in twee blokken: ts-only files met @typescript-eslint/parser, en .vue files met vue-eslint-parser (delegeert <script>-blocks naar @typescript-eslint/parser). Root devDependency vue-eslint-parser toegevoegd. Voorbereiding voor packages die .vue componenten + Vite gebruiken (eerstvolgend gebruiker: ai-sidebar).

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,32 @@
 import tseslint from '@typescript-eslint/eslint-plugin'
 import tsparser from '@typescript-eslint/parser'
+import vueparser from 'vue-eslint-parser'
 
 export default [
   {
-    files: ['packages/*/src/**/*.{ts,vue}'],
+    files: ['packages/*/src/**/*.ts'],
     languageOptions: {
       parser: tsparser,
       parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    files: ['packages/*/src/**/*.vue'],
+    languageOptions: {
+      parser: vueparser,
+      parserOptions: {
+        parser: tsparser,
         ecmaVersion: 'latest',
         sourceType: 'module',
       },
